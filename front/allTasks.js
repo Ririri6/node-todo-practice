7ac05862042bfccb3484e257b05dcff4d958b2f6
Ririri6 <liaiwantian@gmail.com>
2025-08-03 00:20:55 +0900

    削除機能の修正

--- front/allTasks.js
+++ front/allTasks.js
@@ -5,9 +5,37 @@ window.addEventListener("DOMContentLoaded", async () => {
   const tasks = await res.json();
 
   const taskList = document.getElementById("taskList");
-  tasks.forEach((task) => {
-    const li = document.createElement("li");
-    li.textContent = `${task.id}: ${task.task}`;
-    taskList.appendChild(li);
-  });
+  if (tasks.length === 0) {
+    const p = document.createElement("p");
+    p.innerText = "タスクがありません";
+    taskList.appendChild(p);
+  } else {
+    let i = 1;
+    tasks.forEach((task) => {
+      const li = document.createElement("li");
+      const deleteButto = document.createElement("button");
+      deleteButto.innerText = "削除";
+      deleteButto.id = task.id;
+      deleteButto.onclick = () => deleteTask(deleteButto.id);
+      li.textContent = `${task.id}: ${task.task}:`;
+      taskList.appendChild(li);
+      li.appendChild(deleteButto);
+      i++;
+    });
+  }
 });
+
+async function deleteTask(id) {
+  const req = await fetch("api/delete/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+    }),
+  });
+  if (req.status === 200) {
+    window.location.reload();
+  }
+}
